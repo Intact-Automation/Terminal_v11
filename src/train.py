cbef396a79ae5f9dacf6d9eaafa5d6e5aaa29f95
shaurya pratap singh <robotics.shaurya@gmail.com>
2025-08-09 19:14:20 +0530

    Changed file path

--- src/train.py
+++ src/train.py
@@ -5,19 +5,15 @@ model = YOLO('src/config/yolov11-p2.yaml')
 
 # 2️⃣ Train
 model.train(
-    data='src/dataset/data.yaml',   # Dataset YAML
-    epochs=800,                     # Long training for subtle differences
-    batch=256,                      # Large batch size for stability
-    imgsz=1920,                      # High res for small screws
-    workers=16,                      # Speed up dataloading
-    mosaic=1.0,                      # Keep mosaic early
-    close_mosaic=20,                 # Turn off mosaic in last 20 epochs
-    hsv_s=0.6,                       # Saturation aug
-    hsv_v=0.5,                       # Brightness aug
-    grayscale=0.2,                   # 20% grayscale images
-    blur=1.5,                        # Mild blur to simulate focus issues
-    cos_lr=True,                     # Smooth learning rate decay
-    device=0,                        # GPU index
+    data='src/dataset/data.yaml',
+    cfg='src/config/augment.yaml',   # load augmentations from file
+    epochs=800,
+    batch=256,
+    imgsz=1920,
+    workers=16,
+    close_mosaic=20,
+    cos_lr=True,
+    device=0
 )
 
 # 3️⃣ Evaluate model performance on the validation set

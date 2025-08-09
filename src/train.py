27ef7893b84fd821691ac37bac08ae23310d355e
shaurya pratap singh <robotics.shaurya@gmail.com>
2025-08-09 19:17:58 +0530

    Updated the code

--- src/train.py
+++ src/train.py
@@ -1,19 +1,17 @@
 from ultralytics import YOLO
 
-# 1️⃣ Load YOLOv11x-P2 model
 model = YOLO('src/config/yolov11-p2.yaml')
 
-# 2️⃣ Train
 model.train(
     data='src/dataset/data.yaml',
-    cfg='src/config/augment.yaml',   # load augmentations from file
     epochs=800,
     batch=256,
     imgsz=1920,
     workers=16,
     close_mosaic=20,
     cos_lr=True,
-    device=0
+    device=0,
+    hyp='src/config/augment.yaml'   # load this file
 )
 
 # 3️⃣ Evaluate model performance on the validation set

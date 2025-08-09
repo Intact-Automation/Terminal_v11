f766eadaec61f9b6f803389388aa4f08e3cb2702
shaurya pratap singh <robotics.shaurya@gmail.com>
2025-08-09 19:06:28 +0530

    Initial Commit

---
+++ src/dataset/README.dataset.txt
@@ -0,0 +1,6 @@
+# My First Project > 2025-08-01 7:31pm
+https://universe.roboflow.com/intact/my-first-project-uspo1
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
---
+++ src/train.py
@@ -0,0 +1,24 @@
+from ultralytics import YOLO
+
+# 1️⃣ Load YOLOv11x-P2 model
+model = YOLO('yolov11x-p2.yaml')
+
+# 2️⃣ Train
+model.train(
+    data='src/dataset/data.yaml',   # Dataset YAML
+    epochs=800,                     # Long training for subtle differences
+    batch=256,                      # Large batch size for stability
+    imgsz=1920,                      # High res for small screws
+    workers=16,                      # Speed up dataloading
+    mosaic=1.0,                      # Keep mosaic early
+    close_mosaic=20,                 # Turn off mosaic in last 20 epochs
+    hsv_s=0.6,                       # Saturation aug
+    hsv_v=0.5,                       # Brightness aug
+    grayscale=0.2,                   # 20% grayscale images
+    blur=1.5,                        # Mild blur to simulate focus issues
+    cos_lr=True,                     # Smooth learning rate decay
+    device=0,                        # GPU index
+)
+
+# 3️⃣ Evaluate model performance on the validation set
+metrics = model.val()

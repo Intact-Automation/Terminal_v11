741c047db1b9d1b8d0950daa61ded3603372488b
shaurya pratap singh <robotics.shaurya@gmail.com>
2025-08-09 19:23:59 +0530

    Updated the code

--- src/train.py
+++ src/train.py
@@ -5,13 +5,12 @@ model = YOLO('src/config/yolov11-p2.yaml')
 model.train(
     data='src/dataset/data.yaml',
     epochs=800,
-    batch=256,
-    imgsz=1920,
+    batch=32,
     workers=16,
     close_mosaic=20,
     cos_lr=True,
     device=0,
-    hyp='src/config/augment.yaml'   # load this file
+    cfg='src/config/augment.yaml'   # load this file
 )
 
 # 3️⃣ Evaluate model performance on the validation set
